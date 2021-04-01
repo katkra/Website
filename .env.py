bedf0d1f9c9c8b8835098668e3e228831eccb036
Katharina <katkra1994@googlemail.com>
2021-04-01 18:58:33 +0200

    First commit of website project

---
+++ .env.py
@@ -0,0 +1,12 @@
+
+#SQLALCHEMY_TRACK_MODIFICATIONS = False
+#POSTS_PER_PAGE = 3
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS= False
+MAIL_USE_SSL=True
+MAIL_USERNAME = 'katkra1994@googlemail.com'
+MAIL_PASSWORD = 'spannbetttuch7'
+#ADMINS = ['katkra1994@googlemail.com']
+#LANGUAGES = ['en', 'da']
+MS_TRANSLATOR_KEY= 'b03e1786735848589f5b9fdb57fa1868'
---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 3
+    MAIL_SERVER = 'smtp.gmail.com'#os.environ.get('MAIL_SERVER')
+    MAIL_PORT =465#int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS =False #os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL=True
+    MAIL_USERNAME = 'katkra1994@googlemail.com'#os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = 'spannbetttuch7'#os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['katkra1994@googlemail.com']
+    LANGUAGES = ['en', 'da']
+    ELASTICSEARCH_URL = 'http://localhost:9200'#os.environ.get('ELASTICSEARCH_URL')
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+   
